630bf7d02aad2d3aadeac47c7caf6a6dec84c528
YakovMoshel <yakovmoshel123@gmail.com>
2025-05-19 10:40:35 +0300

    Server-side setup

--- src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import CryptoDashboard from '../components/molecules/CryptoDashboard'
-
-export default function HomePage() {
-    return (<>
-        <CryptoDashboard />
-    </>
-    )
-}
